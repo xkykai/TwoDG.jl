761c08d5aed7dd7c6e343252303e7ac3f5a1c1c2
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-02-12 16:06:09 -0500

    barebore structure

--- src/TwoDG.jl
+++ src/TwoDG.jl
@@ -2,4 +2,13 @@ module TwoDG
 
 # Write your package code here.
 
+include("App/App.jl")
+include("Master/Master.jl")
+include("Mesh/Mesh.jl")
+include("Util/Util.jl")
+
+using .App
+using .Master
+using .Mesh
+using .Util
 end
