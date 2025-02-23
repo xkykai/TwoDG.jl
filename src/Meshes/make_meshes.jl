f933819eacbd89a2e0d377d08b7d45edc6cd4e96
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-02-23 17:37:07 -0500

    mesh making in python

---
+++ src/Meshes/make_meshes.jl
@@ -0,0 +1,20 @@
+using CSV
+using DataFrames
+
+function make_circle_mesh(size)
+    TEMP_DIR = "$(@__DIR__)/temp_$(size)"
+    mkpath(TEMP_DIR)
+
+    # @info "Making circle mesh with size $(size) using python"
+    command = `python pyscripts/make_circle_mesh.py $(size) $(TEMP_DIR)`
+    run(command)
+
+    p = Array(CSV.read("$(TEMP_DIR)/p.csv", DataFrame, header=false))
+    t = Array(CSV.read("$(TEMP_DIR)/t.csv", DataFrame, header=false))
+
+    t .+= 1
+
+    # @info "Removing mesh files"
+    rm(TEMP_DIR, recursive=true)
+    return p, t
+end
