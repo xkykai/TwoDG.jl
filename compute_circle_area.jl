5234b06ec08b6ab5d6296e32c49035262876a2f7
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-02-28 16:34:37 -0500

    update script to compute circle area

--- compute_circle_area.jl
+++ compute_circle_area.jl
@@ -4,7 +4,7 @@ using DataFrames
 using CSV
 
 sizs = [0.4, 0.2, 0.1]
-porders = [1, 2, 3]
+porders = [1, 2, 3, 4]
 
 area1s = zeros(length(sizs), length(porders))
 area2s = zeros(length(sizs), length(porders))
@@ -17,9 +17,9 @@ end
 
 #%%
 fig = Figure(size=(1200, 400))
-ax1 = Axis(fig[1, 1], xlabel="p", ylabel="Area difference", title="Area difference vs element order, method 1", yscale=log10)
-ax2 = Axis(fig[1, 2], xlabel="p", ylabel="Area difference", title="Area difference vs element order, method 2", yscale=log10)
-ax3 = Axis(fig[1, 3], xlabel="p", ylabel="Perimeter difference", title="Perimeter difference vs element order", yscale=log10)
+ax1 = Axis(fig[1, 1], xlabel="p", title="|Area 1 - π|", yscale=log10)
+ax2 = Axis(fig[1, 2], xlabel="p", title="|Area 2 - π|", yscale=log10)
+ax3 = Axis(fig[1, 3], xlabel="p", title="|Perimeter - 2π|", yscale=log10)
 
 for j in eachindex(sizs)
     lines!(ax1, porders, abs.(area1s[j, :] .- π), label="Size = $(sizs[j])")
@@ -37,7 +37,6 @@ end
 axislegend(ax3, position=:lb)
 
 display(fig)
-save("./output/area_perimeter_difference.png", fig, px_per_unit=4)
 save("./output/area_perimeter_difference.pdf", fig)
 #%%
 df1 = DataFrame(area1s', :auto)
