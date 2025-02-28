b9e1080fd86003e223db0547095bb72d9a1917bf
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-02-28 16:43:08 -0500

    script to make circle mesh

--- mkmesh_circle.jl
+++ mkmesh_circle.jl
@@ -1,61 +1,10 @@
 using TwoDG
-using CSV
-using DataFrames
 using CairoMakie
-using LinearAlgebra
 
-p, t = make_circle_mesh(0.4)
+siz = 0.4
+porder = 3
+mesh = mkmesh_circle(siz, porder)
 
-p_unique, t_unique = fixmesh(p, t)
-
-meshscatter(p_unique[:, 1], p_unique[:, 2])
-
-f, t2f = mkt2f(t_unique)
-
-boundary(p) = sqrt.(sum(p.^2, dims=2)) .> 1 - 2e-2
-
-bndexpr = [boundary]
-
-f = setbndnbrs(p_unique, f, bndexpr)
-
-fcurved = f[:, 4] .< 0
-tcurved = falses(size(t_unique, 1))
-tcurved[f[fcurved, 3]] .= true
-
-plocal, tlocal = uniformlocalpnts(3)
-
-meshscatter(plocal[:, 2], plocal[:, 3])
-
-mesh = TwoDG.Mesh(p_unique, t_unique, f, t2f, fcurved, tcurved, 3, plocal, tlocal)
-
-#%%
-fd_circle(p) = abs(sqrt(sum(p.^2)) - 1)
-fds = [fd_circle]
-
-mesh = createnodes(mesh, fds)
-
-#%%
-fig = Figure()
-ax = Axis(fig[1, 1], aspect=1)
-for i in 1:size(mesh.dgnodes, 3)
-    scatter!(ax, mesh.dgnodes[:, 1, i], mesh.dgnodes[:, 2, i])
-end
-display(fig)
-#%%
-fig = Figure()
-ax = Axis(fig[1, 1], aspect=1)
-dgnodes = mesh.dgnodes
-tlocal = mesh.tlocal
-for i in 1:size(dgnodes, 3), j in 1:size(tlocal, 1)
-    # lines!(ax, dgnodes[tlocal[j, :], 1, i], dgnodes[tlocal[j, :], 2, i], color=:black)
-    poly!(ax, dgnodes[tlocal[j, :], 1, i], dgnodes[tlocal[j, :], 2, i], strokecolor=:black, strokewidth=0.5)
-end
-
-t = mesh.t
-for i in 1:size(t, 1)
-    poly!(ax, mesh.p[t[i, :], 1], mesh.p[t[i, :], 2], strokewidth=2, strokecolor=:black, color=(:white, 0))
-end
-
-display(fig)
-
+fig = meshplot_curved(mesh, nodes=true, title="Circle mesh, size = $(siz), order = $(porder)", pplot=5)
+save("./output/circle_mesh.png", fig, px_per_unit=8)
 #%%
