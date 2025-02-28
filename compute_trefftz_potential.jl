dbcbad2660ff7d944a97e506985ba2c1df5ab497
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-02-28 16:31:08 -0500

    script to compute trefftz potential

--- compute_trefftz_potential.jl
+++ compute_trefftz_potential.jl
@@ -1,5 +1,6 @@
 using TwoDG
 using CairoMakie
+using Statistics
 
 V∞ = 1
 α = 10
@@ -10,68 +11,42 @@ node_spacing_type = 1
 tparam = [0.1, 0.05, 1.98]
 np_foil = 120
 
-xs, ys, chord = trefftz_points(tparam, np_foil)
+mesh, master, xs_foil, ys_foil, chord, ψ, vx, vy, Γ, CP, CF, Clift, Cdrag, CM, vx_analytical, vy_analytical, Γ_analytical, CP_analytical, CL, CL_analytical = trefftz(V∞, α, m, n, porder, node_spacing_type, tparam)
 
-mesh = mkmesh_trefftz(m, n, porder, node_spacing_type, tparam)
+fig = scaplot(mesh, CP, show_mesh=true, title="Pressure Coefficient, α = $(α)°", limits=quantile(CP, [0.01, 0.99]), figure_size=(800, 700))
+save("./output/cp_trefftz_$(α).png", fig, px_per_unit=8)
 
-c = mesh.dgnodes[:, 1, :] .+ mesh.dgnodes[:, 2, :]
-meshplot(mesh)
-scaplot(mesh, c, show_mesh=true)
+αs = -3:0.5:10
+Clifts = zeros(length(αs))
+Cdrags = zeros(length(αs))
+CLs = zeros(length(αs))
+CL_analyticals = zeros(length(αs))
+CMs = zeros(length(αs))
+
+for (i, α) in enumerate(αs)
+    @info "Computing α = $(α)"
+    mesh, master, xs_foil, ys_foil, chord, ψ, vx, vy, Γ, CP, CF, Clift, Cdrag, CM, vx_analytical, vy_analytical, Γ_analytical, CP_analytical, CL, CL_analytical = trefftz(V∞, α, m, n, porder, node_spacing_type, tparam)
+    Clifts[i] = Clift
+    Cdrags[i] = Cdrag
+    CLs[i] = CL
+    CL_analyticals[i] = CL_analytical
+    CMs[i] = CM
+end
+
 #%%
-fig = Figure(size=(800, 800))
-ax = Axis(fig[1, 1], aspect=1)
-for i in 1:size(mesh.dgnodes, 3), j in 1:size(mesh.tlocal, 1)
-    poly!(ax, mesh.dgnodes[mesh.tlocal[j, :], 1, i], mesh.dgnodes[mesh.tlocal[j, :], 2, i], strokecolor=:black, strokewidth=0.5)
-end
+fig = Figure(size=(1200, 400))
+axCL = Axis(fig[1, 1], xlabel="α (°)", ylabel="Lift coefficient", title="Lift Coefficient vs Angle of Attack")
+axCD = Axis(fig[1, 2], xlabel="α (°)", ylabel="Drag coefficient", title="Drag Coefficient vs Angle of Attack")
+axCM = Axis(fig[1, 3], xlabel="α (°)", ylabel="Moment coefficient", title="Moment Coefficient about 1/4 Chord vs \nAngle of Attack")
 
-t = mesh.t
-for i in 1:size(t, 1)
-    poly!(ax, mesh.p[t[i, :], 1], mesh.p[t[i, :], 2], strokewidth=2, strokecolor=:black, color=(:white, 0))
-end
-
+lines!(axCL, αs, Clifts, label="FE integration")
+lines!(axCL, αs, CL_analyticals, label="Theory (analytical Γ)")
+lines!(axCL, αs, CLs, label="Theory (FE integrated Γ)")
+lines!(axCD, αs, Cdrags, label="FE computation")
+lines!(axCM, αs, CMs, label="FE computation")
+axislegend(axCL, position=:rb)
+axislegend(axCD, position=:lb)
+axislegend(axCM, position=:lb)
 display(fig)
-#%%
-fig = Figure()
-ax = Axis(fig[1, 1]; xlabel="x", ylabel="y")
-scatter!(ax, xs, ys)
-display(fig)
-#%%
-ψ, vx_analytical, vy_analytical, Γ_analytical = potential_trefftz(mesh.dgnodes[:, 1, :], mesh.dgnodes[:, 2, :], V=V∞, alpha=α, tparam=tparam)
-
-scaplot(mesh, ψ, show_mesh=true)
-
-#%%
-trefftz(V∞, α, m, n, porder, node_spacing_type, tparam)
-#%%
-flat_dg = reshape(mesh.dgnodes, :, size(mesh.dgnodes, 2))
-
-scatter(flat_dg[:, 1], flat_dg[:, 2])
-scatter(vec(mesh.dgnodes[:, 1, :]), vec(mesh.dgnodes[:, 2, :]))
-
-#%%
-mesh, master,  ψ, vx, vy, vx_analytical, vy_analytical, Γ_analytical = trefftz(V∞, α, m, n, porder, node_spacing_type, tparam)
-scaplot(mesh, ψ)
-fig = scaplot(mesh, vx, show_mesh=true)
-scaplot(mesh, vx_analytical, show_mesh=true)
-
-scaplot(mesh, vy, show_mesh=true)
-scaplot(mesh, vy_analytical, show_mesh=true)
-#%%
-vx_singularity_locations = findall(x -> abs(x) > 10, vx)
-vy_singulatity_locations = findall(x -> abs(x) > 10, vy)
-#%%
-for i in vx_singularity_locations
-    vx[i] = 0
-end
-
-for i in vy_singulatity_locations
-    vy[i] = 0
-end
-#%%
-fig = scaplot(mesh, vx, show_mesh=true)
-# save("vx_trefftz.png", fig, px_per_unit=4)
-scaplot(mesh, vx_analytical, show_mesh=true)
-
-scaplot(mesh, vy, show_mesh=true)
-scaplot(mesh, vy_analytical, show_mesh=true)
+save("./output/cl_cd_cm_trefftz.pdf", fig)
 #%%
