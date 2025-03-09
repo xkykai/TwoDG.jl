28ab019992ade3451bf53f367e5269fb3130194d
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-03-09 18:05:07 -0400

    reduce repeated jacobian computation

--- src/ContinuousGalerkin/elemmat_cg.jl
+++ src/ContinuousGalerkin/elemmat_cg.jl
@@ -13,10 +13,12 @@ elemmat_cg computes the elemental stiffness matrix and force vector.
 """
 function elemmat_cg(pcg, master, source, param)
     npl = size(pcg, 1)
-    A = zeros(npl, npl)
     κ = param.κ
     c = param.c
     s = param.s
+
+    A = zeros(npl, npl)
+    F = zeros(npl)
 
     for k in eachindex(master.gwgh)
         ϕ = master.shap[:, 1, k]
@@ -32,36 +34,25 @@ function elemmat_cg(pcg, master, source, param)
             ∂ϕᵢ∂y = ∂ϕ∂ξ[i] * invJ[2, 1] + ∂ϕ∂η[i] * invJ[2, 2]
             ∂ϕⱼ∂x = ∂ϕ∂ξ[j] * invJ[1, 1] + ∂ϕ∂η[j] * invJ[1, 2]
             ∂ϕⱼ∂y = ∂ϕ∂ξ[j] * invJ[2, 1] + ∂ϕ∂η[j] * invJ[2, 2]
-            A[i, j] += (∂ϕᵢ∂x * ∂ϕⱼ∂x + ∂ϕᵢ∂y * ∂ϕⱼ∂y) * κ * detJ * master.gwgh[k]
-        end
 
-        # Calculate convection term
-        for i in axes(A, 1), j in axes(A, 2)
-            ∂ϕᵢ∂x = ∂ϕ∂ξ[i] * invJ[1, 1] + ∂ϕ∂η[i] * invJ[1, 2]
-            ∂ϕᵢ∂y = ∂ϕ∂ξ[i] * invJ[2, 1] + ∂ϕ∂η[i] * invJ[2, 2]
             cxϕⱼ = c[1] * ϕ[j]
             cyϕⱼ = c[2] * ϕ[j]
-            A[i, j] += -(∂ϕᵢ∂x * cxϕⱼ + ∂ϕᵢ∂y * cyϕⱼ) * detJ * master.gwgh[k]
-        end
 
-        # Calculate reaction term
-        for i in axes(A, 1), j in axes(A, 2)
+            # Add diffusion term to elemental matrix
+            A[i, j] += (∂ϕᵢ∂x * ∂ϕⱼ∂x + ∂ϕᵢ∂y * ∂ϕⱼ∂y) * κ * detJ * master.gwgh[k]
+
+            # Add convection term to elemental matrixq
+            A[i, j] += -(∂ϕᵢ∂x * cxϕⱼ + ∂ϕᵢ∂y * cyϕⱼ) * detJ * master.gwgh[k]
+
+            # Add reaction term to elemental matrix
             A[i, j] += ϕ[i] * ϕ[j] * s * detJ * master.gwgh[k]
         end
 
-    end
-
-    F = zeros(npl)
-    for j in eachindex(master.gwgh)
-        # Calculate Jacobian matrix at quadrature point
-        J = master.shap[:, 2:3, j]' * pcg
-        
-        # Calculate determinant of Jacobian (represents area scaling factor)
-        detJ = det(J)
-        gauss_coordinate = vec(master.shap[:, 1, j]' * pcg)
+        # Calculate source term
+        gauss_coordinate = vec(master.shap[:, 1, k]' * pcg)
         
         f = source(gauss_coordinate...)
-        F .+= master.shap[:, 1, j] .* detJ .* f .* master.gwgh[j]
+        F .+= master.shap[:, 1, k] .* detJ .* f .* master.gwgh[k]
     end
 
     return A, F
