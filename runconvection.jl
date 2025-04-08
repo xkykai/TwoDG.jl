b106a5311bd7d64ee3bb39a62eb21dcaa361d6cd
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-04-07 23:21:24 -0400

    run convection and plot l2 errors

--- runconvection.jl
+++ runconvection.jl
@@ -1,77 +1,90 @@
 using TwoDG
 using Statistics
-
-m = 10
-n = 10
-porder = 3
+using CairoMakie
 
 time_total = 2π
-dt = 2π / 600
+dt = 2π / 1000
 nstep = 25
 parity = 0
 nodetype = 1
-niter = Int(600 / 25)
+niter = Int(1000 / 25)
 
-mesh = mkmesh_square(m, n, porder, parity, nodetype)
-master = Master(mesh, 2*porder)
+ngrids = [9, 13, 17]
+ps = [2, 3, 4]
 
-mesh2 = deepcopy(mesh)
-master2 = deepcopy(master)
+l2_errors = [zeros(length(ngrids)) for _ in ps]
+l2_errors_distorted = [zeros(length(ngrids)) for _ in ps]
 
-bcm = ones(Int64, 4)
-bcs = zeros(1, 1)
+for (i, ngrid) in enumerate(ngrids), (j, p) in enumerate(ps)
+    m = ngrid
+    n = ngrid
+    porder = p
 
-vf(p) = hcat(-p[:, 2] .+ 0.5, p[:, 1] .- 0.5)
+    mesh = mkmesh_square(m, n, porder, parity, nodetype)
+    master = Master(mesh, 2*porder)
 
-app = mkapp_convection()
-app = App(app; bcm, bcs)
-app.arg[:vf] = vf
+    mesh_distorted = deepcopy(mesh)
+    mkmesh_distort!(mesh_distorted)
 
-init(x, y) = exp(-120 * ((x - 0.6)^2 + (y - 0.5)^2))
+    bcm = ones(Int64, 4)
+    bcs = zeros(1, 1)
 
-u = initu(mesh, app, [init])
+    vf(p) = hcat(-p[:, 2] .+ 0.5, p[:, 1] .- 0.5)
 
-scaplot(mesh, u[:, 1, :], show_mesh=true)
+    app = mkapp_convection()
+    app = App(app; bcm, bcs)
+    app.arg[:vf] = vf
 
+    init(x, y) = exp(-120 * ((x - 0.6)^2 + (y - 0.5)^2))
 
-a = rinvexpl(master, mesh, app, u, 0)
-scaplot(mesh, a[:, 1, :], show_mesh=true, cmap=:viridis)
-# #%%
-tm = 0.
+    u = initu(mesh, app, [init])
+    @info "Computing MSE for p = $(p) and size = $(ngrids[i])"
 
-for i in 1:niter
-    tm += nstep * dt
-    # println("Time: ", tm)
-    
-    # Update the solution using the convection operator
-    # u .= rk4(rinvexpl, master, mesh, app, u, tm, dt, nstep)
-    rk4!(rinvexpl, master, mesh, app, u, tm, dt, nstep)
-    fig = scaplot(mesh, u[:, 1, :], show_mesh=true, cmap=:viridis)
-    display(fig)
+    tm = 0.
+    for i in 1:niter
+        # println("Time: ", tm)
+        
+        # Update the solution using the convection operator
+        # u .= rk4(rinvexpl, master, mesh, app, u, tm, dt, nstep)
+        rk4!(rinvexpl, master, mesh, app, u, tm, dt, nstep)
+        # fig = scaplot(mesh, u[:, 1, :], show_mesh=true, cmap=:viridis)
+        # display(fig)
+        tm += nstep * dt
+    end
+    l2_errors[j][i] = l2_error(mesh, u, init)
+
+    @info "Computing MSE for distorted mesh p = $(p) and size = $(ngrids[i])"
+
+    u_distorted = initu(mesh_distorted, app, [init])
+    tm = 0.
+    for i in 1:niter
+        # println("Time: ", tm)
+        
+        # Update the solution using the convection operator
+        # u .= rk4(rinvexpl, master, mesh, app, u, tm, dt, nstep)
+        rk4!(rinvexpl, master, mesh_distorted, app, u_distorted, tm, dt, nstep)
+        # fig = scaplot(mesh_distorted, u[:, 1, :], show_mesh=true, cmap=:viridis)
+        # display(fig)
+        tm += nstep * dt
+    end
+    l2_errors_distorted[j][i] = l2_error(mesh_distorted, u_distorted, init)
 end
 #%%
-mkmesh_distort!(mesh2)
-meshplot_curved(mesh2, pplot=6)
-#%%
-a = rinvexpl(master, mesh, app, u, 0)
-scaplot(mesh, a[:, 1, :], show_mesh=true, cmap=:viridis)
-#%%
-a2 = rinvexpl(master2, mesh2, app, u, 0)
-scaplot(mesh2, a2[:, 1, :], show_mesh=true, cmap=:viridis)
-#%%
-tm = 0.
-u2 = initu(mesh2, app, [init])
-
-for i in 1:niter
-    tm += nstep * dt
-    println("Time: ", tm)
-    
-    # Update the solution using the convection operator
-    # u .= rk4(rinvexpl, master, mesh, app, u, tm, dt, nstep)
-    rk4!(rinvexpl, master2, mesh2, app, u2, tm, dt, nstep)
-    fig = scaplot(mesh2, u2[:, 1, :], show_mesh=true, cmap=:viridis)
-    display(fig)
+fig = Figure()
+ax = Axis(fig[1, 1], xlabel="1/h", ylabel="L2 norm", title="Error convergence of DG explicit linear convection", yscale=log10, xscale=log2)
+for i in eachindex(ngrids)
+    scatterlines!(ax, ngrids .- 1, l2_errors[i], label="p=$(ps[i])")
 end
+axislegend(ax, position=:lb)
+display(fig)
+save("./output/dg_convection_convergence.png", fig, px_per_unit=8)
 #%%
-mean((u2 .- initu(mesh2, app, [init])).^2)
+fig = Figure()
+ax = Axis(fig[1, 1], xlabel="1/h", ylabel="L2 norm", title="Error convergence of DG explicit linear convection (distorted mesh)", yscale=log10, xscale=log2)
+for i in eachindex(ngrids)
+    scatterlines!(ax, ngrids .- 1, l2_errors_distorted[i], label="p=$(ps[i])")
+end
+axislegend(ax, position=:lb)
+display(fig)
+save("./output/dg_convection_convergence_distorted.png", fig, px_per_unit=8)
 #%%
