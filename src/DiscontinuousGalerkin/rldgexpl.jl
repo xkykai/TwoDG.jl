5a3e75dabf41168672f94f6774ec87523328b6a1
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-04-14 22:33:04 -0400

    fix translation bug, write getq, use multithreading

--- src/DiscontinuousGalerkin/rldgexpl.jl
+++ src/DiscontinuousGalerkin/rldgexpl.jl
@@ -1,7 +1,5 @@
 using LinearAlgebra
 
-using LinearAlgebra
-
 """
     getq(master, mesh, app, u, time)
 
@@ -29,12 +27,12 @@ function getq(master, mesh, app, u, time)
     q = zeros((npl, 2, nc, nt))
     
     # Interfaces
-    sh1d = dropdims(master.sh1d[:, 1, :], dims=2)  # Julia uses 1-based indexing
+    sh1d = master.sh1d[:, 1, :] # Julia uses 1-based indexing
     perm = master.perm
-    ni = findfirst(x -> x < 0, mesh.f[:, 4])  # Julia uses 1-based indexing
+    ni = findfirst(x -> x < 0, mesh.f[:, 4]) - 1  # Julia uses 1-based indexing
     
     # Interior first
-    for i in 1:ni
+    Threads.@threads for i in 1:ni
         ipt = sum(mesh.f[i, 1:2])
         el = mesh.f[i, 3]
         er = mesh.f[i, 4]
@@ -55,19 +53,39 @@ function getq(master, mesh, app, u, time)
             ior = 2  # Julia uses 1-based indexing
         end
         
-        perml = dropdims(perm[:, isl, iol], dims=2)
-        permr = dropdims(perm[:, isr, ior], dims=2)
+        perml = perm[:, isl, iol]
+        permr = perm[:, isr, ior]
         
         # INSERT CODE HERE .....
+        if mesh.fcurved[i]
+            xxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 1, el])
+            yxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 2, el])
+            dsdxi = sqrt.(xxi.^2 + yxi.^2)
+            nl = hcat(yxi./dsdxi, -xxi./dsdxi)
+            dws = master.gw1d .* dsdxi
+        else
+            dx = mesh.p[mesh.f[i, 2], :] - mesh.p[mesh.f[i, 1], :]
+            dsdxi = sqrt(dx[1]^2 + dx[2]^2)
+            nl = repeat([dx[2], -dx[1]]./dsdxi, 1, ng1d)'
+            dws = master.gw1d .* dsdxi
+        end
+
+        ul = @view(u[perml, :, el])
+        ulg = sh1d' * ul
+
+        û = ulg
         
-        q[perml, 1, :, el] = q[perml, 1, :, el] + cntx
-        q[perml, 2, :, el] = q[perml, 2, :, el] + cnty
-        q[permr, 1, :, er] = q[permr, 1, :, er] - cntx
-        q[permr, 2, :, er] = q[permr, 2, :, er] - cnty
+        cntx = sh1d * Diagonal(dws .* nl[:, 1]) * û
+        cnty = sh1d * Diagonal(dws .* nl[:, 2]) * û
+
+        q[perml, 1, :, el] .+= cntx
+        q[perml, 2, :, el] .+= cnty
+        q[permr, 1, :, er] .-= cntx
+        q[permr, 2, :, er] .-= cnty
     end
-    
+
     # Now boundary
-    for i in (ni+1):size(mesh.f, 1)
+    Threads.@threads for i in (ni+1):size(mesh.f, 1)
         ipt = sum(mesh.f[i, 1:2])
         el = mesh.f[i, 3]
         ib = -mesh.f[i, 4]  # Keep 0-based for the ib index since we're using it as array index
@@ -80,16 +98,39 @@ function getq(master, mesh, app, u, time)
             iol = 2  # Julia uses 1-based indexing
         end
         
-        perml = dropdims(perm[:, isl, iol], dims=2)
+        perml = perm[:, isl, iol]
         
         # INSERT CODE HERE .....
+        if app.pg
+            plg = sh1d' * mesh.dgnodes[perml, :, el]
+        else
+            plg = []
+        end
+
+        # Get solution values on boundary element
+        ul = u[perml, :, el]
+        ulg = sh1d' * ul  # Interpolate solution to quadrature points
+
+        if mesh.fcurved[i]
+            xxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 1, el])
+            yxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 2, el])
+            dsdxi = sqrt.(xxi.^2 + yxi.^2)
+            nl = hcat(yxi./dsdxi, -xxi./dsdxi)
+            dws = master.gw1d .* dsdxi
+        else
+            dx = mesh.p[mesh.f[i, 2], :] - mesh.p[mesh.f[i, 1], :]
+            dsdxi = sqrt(dx[1]^2 + dx[2]^2)
+            nl = repeat([dx[2], -dx[1]]./dsdxi, 1, ng1d)'
+            dws = master.gw1d .* dsdxi
+        end
         
-        ub = app.fvisub(...)  # This line is incomplete in the original code
-        cntx = sh1d * Diagonal(master.gw1d .* dsdxi .* nl[:, 1]) * sh1d' * ub
-        cnty = sh1d * Diagonal(master.gw1d .* dsdxi .* nl[:, 2]) * sh1d' * ub
+        ub = app.fvisub(ulg, nl, app.bcm[ib], app.bcs[app.bcm[ib], :], plg, app.arg, time)  # This line is incomplete in the original code
+
+        cntx = sh1d * Diagonal(master.gw1d .* dsdxi .* nl[:, 1]) * ub
+        cnty = sh1d * Diagonal(master.gw1d .* dsdxi .* nl[:, 2]) * ub
         
-        q[perml, 1, :, el] = q[perml, 1, :, el] + cntx
-        q[perml, 2, :, el] = q[perml, 2, :, el] + cnty
+        q[perml, 1, :, el] .+= cntx
+        q[perml, 2, :, el] .+= cnty
     end
     
     # Volume integral
@@ -99,11 +140,38 @@ function getq(master, mesh, app, u, time)
     shapxig = shapxi * Diagonal(master.gwgh)
     shapetg = shapet * Diagonal(master.gwgh)
     
-    for i in 1:nt
+    Threads.@threads for i in 1:nt
         # INSERT CODE HERE .....
+        # Set up the coordinates and Jacobian for volume integration
+        if mesh.tcurved[i]
+            # For curved elements, compute Jacobian at each quadrature point
+            xxi = @view(mesh.dgnodes[:, 1, i])' * shapxi
+            xet = @view(mesh.dgnodes[:, 1, i])' * shapet
+            yxi = @view(mesh.dgnodes[:, 2, i])' * shapxi
+            yet = @view(mesh.dgnodes[:, 2, i])' * shapet
+            jac = xxi .* yet - xet .* yxi
+            shapx = shapxig * Diagonal(yet) - shapetg * Diagonal(yxi)
+            shapy = -shapxig * Diagonal(xet) + shapetg * Diagonal(xxi)
+            M = shap * Diagonal(master.gwgh .* jac) * shap'
+        else
+            # For straight elements, compute constant Jacobian
+            xxi = mesh.p[mesh.t[i, 2], 1] - mesh.p[mesh.t[i, 1], 1]
+            xet = mesh.p[mesh.t[i, 3], 1] - mesh.p[mesh.t[i, 1], 1]
+            yxi = mesh.p[mesh.t[i, 2], 2] - mesh.p[mesh.t[i, 1], 2]
+            yet = mesh.p[mesh.t[i, 3], 2] - mesh.p[mesh.t[i, 1], 2]
+            jac = xxi * yet - xet * yxi
+            shapx = shapxig * yet - shapetg * yxi
+            shapy = -shapxig * xet + shapetg * xxi
+            M = master.mass * jac
+        end
+
+        # Calculate the contribution to gradient from the element interior
+        # Form the matrices for computing gradients in x and y directions
+        Cx = (shapx * shap')'
+        Cy = (shapy * shap')'
         
-        q[:, 1, :, i] = q[:, 1, :, i] - Cx' * @view(u[:, :, i])
-        q[:, 2, :, i] = q[:, 2, :, i] - Cy' * @view(u[:, :, i])
+        q[:, 1, :, i] .-= Cx' * @view(u[:, :, i])
+        q[:, 2, :, i] .-= Cy' * @view(u[:, :, i])
         
         q_reshaped = reshape(q[:, :, :, i], (npl, 2*nc))
         q[:, :, :, i] = reshape(M \ q_reshaped, (npl, 2, nc))
@@ -140,19 +208,19 @@ function rldgexpl(master, mesh, app, u, time)
     ng1d = length(master.gw1d)
 
     q = nothing
-    if app.fvisv
+    if app.fvisv !== nothing
         q = getq(master, mesh, app, u, time)
     end
 
     r = zero(u)
 
     # Interfaces
-    sh1d = dropdims(master.sh1d[:, 1, :], dims=2)  # Julia uses 1-based indexing
+    sh1d = master.sh1d[:, 1, :]# Julia uses 1-based indexing
     perm = master.perm
-    ni = findfirst(x -> x < 0, mesh.f[:, 4])  # Julia uses 1-based indexing
+    ni = findfirst(x -> x < 0, mesh.f[:, 4]) - 1  # Julia uses 1-based indexing
 
     # Interior first
-    for i in 1:ni
+    Threads.@threads for i in 1:ni
         ipt = sum(mesh.f[i, 1:2])
         el = mesh.f[i, 3]
         er = mesh.f[i, 4]
@@ -173,15 +241,15 @@ function rldgexpl(master, mesh, app, u, time)
             ior = 2  # Julia uses 1-based indexing
         end
         
-        perml = dropdims(perm[:, isl, iol], dims=2)
-        permr = dropdims(perm[:, isr, ior], dims=2)
+        perml = perm[:, isl, iol]
+        permr = perm[:, isr, ior]
 
         # Use conditional ? syntax for shorter code
         plg = app.pg ? sh1d' * @view(mesh.dgnodes[perml, :, el]) : []
 
         if mesh.fcurved[i]
-            xxi = dropdims(master.sh1d[:, 2, :], dims=2)' * dropdims(@view(mesh.dgnodes[perml, 1, el]), dims=2)
-            yxi = dropdims(master.sh1d[:, 2, :], dims=2)' * dropdims(@view(mesh.dgnodes[perml, 2, el]), dims=2)
+            xxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 1, el])
+            yxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 2, el])
             dsdxi = sqrt.(xxi.^2 + yxi.^2)
             nl = hcat(yxi./dsdxi, -xxi./dsdxi)
             dws = master.gw1d .* dsdxi
@@ -200,7 +268,7 @@ function rldgexpl(master, mesh, app, u, time)
 
         fng = app.finvi(ulg, urg, nl, plg, app.arg, time)
 
-        if app.fvisv
+        if app.fvisv !== nothing
             qr = @view(q[permr, :, :, er])
             qrg = reshape(sh1d' * reshape(qr, (np1d, 2*nc)), (ng1d, 2, nc))
             fnvg = app.fvisi(ulg, urg, nothing, qrg, nl, plg, app.arg, time)
@@ -215,7 +283,7 @@ function rldgexpl(master, mesh, app, u, time)
     end
 
     # Now Boundary
-    for i in (ni+1):size(mesh.f, 1)
+    Threads.@threads for i in (ni+1):size(mesh.f, 1)
         ipt = sum(mesh.f[i, 1:2])
         el = mesh.f[i, 3]
         ib = -mesh.f[i, 4]  # Keep 0-based for the ib index since we're using it as array index
@@ -228,13 +296,13 @@ function rldgexpl(master, mesh, app, u, time)
             iol = 2
         end
 
-        perml = dropdims(perm[:, isl, iol], dims=2)
+        perml = perm[:, isl, iol]
 
         plg = app.pg ? sh1d' * @view(mesh.dgnodes[perml, :, el]) : []
 
         if mesh.fcurved[i]
-            xxi = dropdims(master.sh1d[:, 2, :], dims=2)' * dropdims(@view(mesh.dgnodes[perml, 1, el]), dims=2)
-            yxi = dropdims(master.sh1d[:, 2, :], dims=2)' * dropdims(@view(mesh.dgnodes[perml, 2, el]), dims=2)
+            xxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 1, el])
+            yxi = master.sh1d[:, 2, :]' * @view(mesh.dgnodes[perml, 2, el])
             dsdxi = sqrt.(xxi.^2 + yxi.^2)
             nl = hcat(yxi./dsdxi, -xxi./dsdxi)
             dws = master.gw1d .* dsdxi
@@ -248,12 +316,12 @@ function rldgexpl(master, mesh, app, u, time)
         ul = @view(u[perml, :, el])
         ulg = sh1d' * ul
         
-        fng = app.finvb(ulg, nl, app.bcm[ib+1], app.bcs[app.bcm[ib+1], :], plg, app.arg, time)
+        fng = app.finvb(ulg, nl, app.bcm[ib], app.bcs[app.bcm[ib], :], plg, app.arg, time)
 
-        if app.fvisv
+        if app.fvisv !== nothing
             ql = @view(q[perml, :, :, el])
             qlg = reshape(sh1d' * reshape(ql, (np1d, 2*nc)), (ng1d, 2, nc))
-            fnvg = app.fvisb(ulg, qlg, nl, app.bcm[ib+1], app.bcs[app.bcm[ib+1], :], plg, app.arg, time)
+            fnvg = app.fvisb(ulg, qlg, nl, app.bcm[ib], app.bcs[app.bcm[ib], :], plg, app.arg, time)
             fng = fng + fnvg
         end
         
@@ -270,7 +338,7 @@ function rldgexpl(master, mesh, app, u, time)
     shapxig = shapxi * Diagonal(master.gwgh)
     shapetg = shapet * Diagonal(master.gwgh)
 
-    for i in 1:nt
+    Threads.@threads for i in 1:nt
         pg = app.pg ? shap' * @view(mesh.dgnodes[:, :, i]) : []
 
         if mesh.tcurved[i]
@@ -295,14 +363,14 @@ function rldgexpl(master, mesh, app, u, time)
 
         ug = shap' * @view(u[:, :, i])
 
-        if app.src
+        if app.src !== nothing
             src = app.src(ug, [], pg, app.arg, time)
             r[:, :, i] = r[:, :, i] + shap * Diagonal(master.gwgh .* jac) * src
         end
 
         fgx, fgy = app.finvv(ug, pg, app.arg, time)
 
-        if app.fvisv
+        if app.fvisv !== nothing
             qg = reshape(shap' * reshape(@view(q[:, :, :, i]), (npl, 2*nc)), (ng, 2, nc))
             fxvg, fyvg = app.fvisv(ug, qg, pg, app.arg, time)
             fgx = fgx + fxvg
