d821f767fbe11e9cff235d35ea2b526848a585bd
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-04-18 23:55:46 -0400

    run poisson's equation on circle

--- run_ldg_circle.jl
+++ run_ldg_circle.jl
@@ -30,23 +30,43 @@ app.arg[:c11int] = c11int
 
 
 time_total = 2
-nstep = 128
+nstep = 32
 dt = time_total / (nstep * 250)
 ncycl = Int(ceil(time_total / dt / nstep))
+
+function poisson_solution(x, y)
+    r² = x^2 + y^2
+    return 0.25 * (1 - r²)
+end
 #%%
-size = 0.25
-porder = 3
-mesh = mkmesh_circle(size, porder, 1)
-master = Master(mesh, 4*porder)
+porders = [1, 2]
+sizes = [0.4, 0.25]
 
-meshplot_curved(mesh)
-u = initu(mesh, app, [0])
+l2_errors = [zeros(length(sizes)) for _ in porders]
+for (i, porder) in enumerate(porders), (j, size) in enumerate(sizes)
+    @info "Computing MSE for p = $(porder) and size = $(size)"
+    mesh = mkmesh_circle(size, porder, 1)
+    master = Master(mesh, 4*porder)
+    u = initu(mesh, app, [0])
 
-time = 0
-for i in 1:ncycl
-    @info "time = $(time)"
-    rk4!(rldgexpl, master, mesh, app, u, time, dt, nstep)
-    time += nstep * dt
+    time = 0
+    for i in 1:ncycl
+        @info "time = $(time)"
+        rk4!(rldgexpl, master, mesh, app, u, time, dt, nstep)
+        time += nstep * dt
+    end
+
     fig = scaplot(mesh, u[:, 1, :], show_mesh=true)
     display(fig)
+    l2_errors[i][j] = l2_error(mesh, u, poisson_solution)
 end
+#%%
+fig = Figure()
+ax = Axis(fig[1, 1], xlabel="1/h", ylabel="L2 norm", title="Error convergence of LDG, circular domain", yscale=log10, xscale=log2)
+for i in eachindex(porders)
+    scatterlines!(ax, 1 ./ sizes, l2_errors[i], label="p = $(porders[i])")
+end
+axislegend(ax, position=:lb)
+display(fig)
+# save("./output/ldg_circle_convergence.png", fig, px_per_unit=8)
+#%%
