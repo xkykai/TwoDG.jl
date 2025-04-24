d02b5a285acb8065f2d72c96280ffe5fa30670da
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-04-24 14:52:47 -0400

    refactor runhdg.jl to iterate over multiple mesh sizes and coefficients for HDG solution

--- runhdg.jl
+++ runhdg.jl
@@ -1,36 +1,36 @@
 using TwoDG
+using CairoMakie
 
 porder = 4
 ngauss = 2 * (porder + 1)
-siz = 0.1
 boundary_refinement = 3
 
-mesh = mkmesh_circle(siz, porder, 1; boundary_refinement)
-master = Master(mesh, ngauss)
-meshplot_curved(mesh)
-
-mesh1 = make_circle_nodes(mesh.p, mesh.t, porder + 1, 1)
-master1 = Master(mesh1, ngauss)
-
-meshplot_curved(mesh1)
-#%%
-kappa = 1
-c = [100, 100]
-taud = 1
-
-param = Dict(:kappa => kappa, :c => c, :taud => taud)
-hdg_source(p) = 10.0 .* ones(size(p, 1), 1)
+hdg_source(p) = ones(size(p, 1), 1)
 dbc(p) = zeros(size(p, 1), 1)
 
-# uh, qh, uhath = hdg_solve(master, mesh, hdg_source, dbc, param)
-u, q, uh = hdg_solve(master, mesh, hdg_source, dbc, param)
+kappa = 1
+taud = 1
+sizs = [0.25, 0.2, 0.1]
+cs = [[1, 1], [10, 10], [100, 100]]
 
-fig = scaplot(mesh, u[:, 1, :], show_mesh=true)
-save("output/hdg_circle_u.png", fig, px_per_unit=8)
-# scaplot(mesh, -q[:, 2, :], show_mesh=true, title="q")
-#%%
-ustarh = hdg_postprocess(master, mesh, master1, mesh1, u, q ./ kappa)
+for siz in sizs
+    mesh = mkmesh_circle(siz, porder, 1; boundary_refinement)
+    master = Master(mesh, ngauss)
+    
+    mesh1 = make_circle_nodes(mesh.p, mesh.t, porder + 1, 1)
+    master1 = Master(mesh1, ngauss)
+    
+    for c in cs
+        @info "Running for size = $siz and c = $c"
+        param = Dict(:kappa => kappa, :c => c, :taud => taud)
 
-scaplot(mesh1, ustarh[:, 1, :], show_mesh=true, title="u*")
-scaplot(mesh, u[:, 1, :], show_mesh=true, title="u", limits=extrema(u[:, 1, :]))
-#%%
+        u, q, uh = hdg_solve(master, mesh, hdg_source, dbc, param)
+        ustarh = hdg_postprocess(master, mesh, master1, mesh1, u, q ./ kappa)
+
+        fig = scaplot(mesh, u[:, 1, :], show_mesh=true, title="u")
+        save("output/hdg_convdiff_u_size_$(siz)_k_$(kappa)_c_$(c[1])_$(c[2])_p_$(porder).png", fig, px_per_unit=4)
+
+        fig = scaplot(mesh1, ustarh[:, 1, :], show_mesh=true, title="u*")
+        save("output/hdg_convdiff_ustar_size_$(siz)_k_$(kappa)_c_$(c[1])_$(c[2])_p_$(porder).png", fig, px_per_unit=4)
+    end
+end
